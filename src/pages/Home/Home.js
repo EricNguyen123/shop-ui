c7787ebe19b7e902fda4cfbb5b40f0f143e2ea87
Your Name <huyhuhyhoa@gmail.com>
2023-07-14 01:32:30 +0700

    slider

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,16 @@
+import classNames from 'classnames/bind';
+import styles from './Home.module.scss';
+
+import Banner from './component/Banner';
+
+const cx = classNames.bind(styles);
+
+function Home() {
+    return (
+        <div className={cx('wrapper')}>
+            <Banner />
+        </div>
+    );
+}
+
+export default Home;
