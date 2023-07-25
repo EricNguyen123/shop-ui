d30a592dde2a0ad2a12710f1b131454ec44c2756
Your Name <huyhuhyhoa@gmail.com>
2023-07-25 15:13:58 +0700

    a

--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -9,24 +9,23 @@ import OverView from './component/OverView';
 import CrossSells from './component/CrossSells';
 import TabDetail from './component/TabDetail';
 import Description from './component/Description';
-// import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const cx = classNames.bind(styles);
 
 function ProductDetail({ directive, title, item }) {
-    // const [imgs, setImgs] = useState([]);
-    // for (var i = 0; i < item.color.listColor.length; i++) {
-    //     if (item.color.listColor[i].colorName === 'space-black') {
-    //         return setImgs(item.color.listColor[i].img);
-    //     }
-    // }
+    const [imgs, setImgs] = useState([]);
+    useEffect(() => {
+        const dataImgs = item.color.listColor.find((data) => data.colorName === 'space-black');
+        setImgs(dataImgs.img);
+    }, [item.color.listColor]);
 
     return (
         <div className={cx('wrapper')}>
             <Breadcrumb directive={directive} title={title} />
             <div className={cx('inner')}>
                 <div className={cx('assential')}>
-                    <ProductAssential className={cx('review-img')} dataImg={item.color.listColor[0].img} />
+                    <ProductAssential className={cx('review-img')} dataImg={imgs} />
                     <OverView className={cx('review-content')} />
                 </div>
                 <CrossSells />
